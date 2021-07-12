4df14d652fa7f62de64c8cfe95405a94f03fb397
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-07-12 13:00:54 -0500

    minor changes to date functionality

--- controllers/performers.js
+++ controllers/performers.js
@@ -6,13 +6,8 @@ export {
 }
 
 function create(req, res) {
-  // Needed to "fix" date formatting to prevent day off by 1
-  // This is due to the <input type="date"> returning the date
-  // string in this format:  "YYYY-MM-DD"
-  // https://stackoverflow.com/questions/7556591/is-the-javascript-date-object-always-one-day-off
-  let birthDate = new Date(req.body.born)
-  req.body.born = birthDate.toUTCString()
   Performer.create(req.body, function (err, performer) {
+    console.log(performer)
     res.redirect('/performers/new')
   })
 }
